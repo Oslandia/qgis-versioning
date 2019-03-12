7f4c31075f131063c8291b24f1fc75e5e8efe316
Julien Cabieces <julien.cabieces@oslandia.com>
2019-03-12 08:31:29 +0100

    Modify test database creation script to - Use user postgres instead of julien - Don't create versioning tables

--- test/create_db_test.sh
+++ test/create_db_test.sh
@@ -4,14 +4,14 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 for db in epanet_test_db epanet_test_db_copy;
 do
-    dropdb --if-exists -h 127.0.01 -U julien $db
-    createdb -h 127.0.01 -U julien $db
-    psql -h 127.0.01 -U julien $db -c 'CREATE EXTENSION postgis'
-    psql -h 127.0.01 -U julien $db -f $SCRIPT_DIR/epanet_test_db.sql
+    dropdb --if-exists -h 127.0.01 -U postgres $db
+    createdb -h 127.0.01 -U postgres $db
+    psql -h 127.0.01 -U postgres $db -c 'CREATE EXTENSION postgis'
+    psql -h 127.0.01 -U postgres $db -f $SCRIPT_DIR/epanet_test_db_wo_versioning.sql
 done
 
 EMPTY_DB="qgis_versioning_empty_db"
-dropdb --if-exists -h 127.0.01 -U julien $EMPTY_DB
-createdb -h 127.0.01 -U julien $EMPTY_DB
-psql -h 127.0.01 -U julien $EMPTY_DB -c 'CREATE EXTENSION postgis'
+dropdb --if-exists -h 127.0.01 -U postgres $EMPTY_DB
+createdb -h 127.0.01 -U postgres $EMPTY_DB
+psql -h 127.0.01 -U postgres $EMPTY_DB -c 'CREATE EXTENSION postgis'
 

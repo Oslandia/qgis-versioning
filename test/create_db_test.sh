d7eb1f655b08e309fd1b681ec87770dc4a7561f7
Julien Cabieces <julien.cabieces@oslandia.com>
2019-03-22 16:03:03 +0100

    Remove useless sql file

--- test/create_db_test.sh
+++ test/create_db_test.sh
@@ -7,7 +7,7 @@ do
     dropdb --if-exists -h 127.0.01 -U postgres $db
     createdb -h 127.0.01 -U postgres $db
     psql -h 127.0.01 -U postgres $db -c 'CREATE EXTENSION postgis'
-    psql -h 127.0.01 -U postgres $db -f $SCRIPT_DIR/epanet_test_db_wo_versioning.sql
+    psql -h 127.0.01 -U postgres $db -f $SCRIPT_DIR/epanet_test_db.sql
 done
 
 EMPTY_DB="qgis_versioning_empty_db"
